b727431f39efa76b7594e67beb9807f17dc3bb3e
emomonkey <stephen.emo@gmail.com>
2018-08-21 22:57:15 +0100

    Add Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule, MatCardModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
