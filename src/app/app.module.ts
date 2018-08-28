3e06602d2342cc1fb816ed5d389720bf86cdc17f
emomonkey <stephen.emo@gmail.com>
2018-08-28 22:12:45 +0100

    Completed Shotgunsz tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatCardModule, MatProgressSpinnerModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,8 @@ import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
